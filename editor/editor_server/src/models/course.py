4d574ecbf708c88c4b17b1c73263521a6ef62608
David <davidvoler@gmail.com>
2025-12-14 13:28:26 +0200

    Starting to work on the server side

---
+++ editor/editor_server/src/models/course.py
@@ -0,0 +1,84 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+
+
+class Question(BaseModel):
+    id: int
+    step_id: int
+    sentence_id: int
+    explanation: str = ''
+    weight: int = 0
+
+
+class Step(BaseModel):
+    id: int
+    lesson_id: int
+    name: str
+    description: str = ''
+    created_by: str
+    school: str = ''
+    public: bool = True
+    reviewed: bool = False
+    questions: list[Question]
+    created_at: datetime = datetime.now()
+    updated_at: datetime = datetime.now()
+
+class Lesson(BaseModel):
+    id: int
+    course_id: int
+    name: str
+    description: str = ''
+    created_by: str = ''
+    school: str = ''
+    public: bool = True
+    reviewed: bool = False
+    steps: list[Step]
+    created_at: datetime = datetime.now()
+    updated_at: datetime = datetime.now()
+
+
+class Course(BaseModel):
+    id: int
+    name: str
+    description: str = ''
+    lang: str
+    to_lang: str
+    created_by: str
+    school: str = ''
+    public: bool = True
+    reviewed: bool = False
+    lessons: list[Lesson]
+    created_at: datetime = datetime.now()
+    updated_at: datetime = datetime.now()
+
+class CourseRequest(BaseModel):
+    lang: str
+    to_lang: str
+    name: str
+    description: str = ''
+    limit: int = 300
+    offset: int = 0
+    created_by: str
+    school: str = ''
+
+class GenerateRequest(BaseModel):
+    lang: str
+    to_lang: str
+    name: str
+    description: str = ''
+    created_by: str
+    school: str = ''
+    level: int = 0
+    words: list[str]
+    verbs: list[str]
+
+class GenerateLessonsRequest(GenerateRequest):
+    course_id: int
+    lesson_count: int
+    
+class GenerateQuestionRequest(GenerateRequest):
+    lesson_id: int
+    step_count: int
+
--- editor/editor_server/src/routers/course.py
+++ editor/editor_server/src/routers/course.py
@@ -1,1 +1,13 @@
-from 
+from fastapi import APIRouter
+from models.course import Course, CourseRequest
+from utils.db import get_query_results
+
+router = APIRouter()
+
+@router.post("/", response_model=list[Course])
+async def list_courses(request: CourseRequest):
+    sql = """
+    SELECT * FROM content_raw.courses WHERE lang = %s AND to_lang = %s
+    """
+    data = await get_query_results(sql, (request.lang, request.to_lang, request.limit, request.offset))
+    return [Course(**row) for row in data]
