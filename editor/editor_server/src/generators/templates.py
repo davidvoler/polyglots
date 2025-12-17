2a7d37d3122dc202b1990128097bde3de8bc476d
David <davidvoler@gmail.com>
2025-12-17 17:21:02 +0200

    initial templates

--- editor/editor_server/src/generators/templates.py
+++ editor/editor_server/src/generators/templates.py
@@ -19,6 +19,34 @@ class LessonTemplate(BaseModel):
 
 
 
+class CourseTemplate(BaseModel):
+    name: str
+    description: str
+    modules: list[ModuleTemplate]
+    def get_template_data(self):
+        module_count = 0
+        lesson_count = 0
+        lessons_types = set()
+        lessons_content = set()
+        for mt in self.modules:
+            module_count += mt.module_count
+            lesson_count += mt.lessons_count * mt.module_count
+            lessons_types.update(mt.lessons_types)
+            lessons_content.update(mt.lessons_content)
+        
+
+        
+        return {
+            "name": self.name,
+            "description": self.description,
+            "modules_groups": len(self.modules),
+            "modules_count": module_count,
+            "lessons_count": lesson_count,
+            "lessons_types": list(lessons_types),
+            "lessons_content": list(lessons_content),
+        }
+
+
 COURSE_TEMPLATE1 = [
     ModuleTemplate(words=10, level=1, lessons_types=['listening', 'vocabulary'], lessons_content=['greetings'], lessons_count=10, module_count=5),
     ModuleTemplate(words=15, level=2, lessons_types=['listening', 'vocabulary'], lessons_content=[ 'verbs', 'nouns',], lessons_count=10, module_count=5),
@@ -27,7 +55,11 @@ COURSE_TEMPLATE1 = [
 
 
 
-COURSE_TEMPLATE_JAPANESE = [
+
+COURSE_TEMPLATE_JAPANESE = CourseTemplate(
+    name="Japanese Course",
+    description="A course for learning Japanese including learning to read and write Hiragana, Kanji and Katakana",
+    modules=[
     ModuleTemplate(words=10, level=1, lessons_types=['listening', 'vocabulary'], lessons_content=['greetings'], lessons_count=10, module_count=5),
     ModuleTemplate(words=20, level=5, lessons_types=['listening', 'vocabulary'], lessons_content=[ 'verbs', 'nouns',], lessons_count=10, module_count=5),
     ModuleTemplate(words=15, level=10, lessons_types=['reading', 'alphabet'], lessons_content=[ 'hiragana'], lessons_count=10, module_count=5),
@@ -49,22 +81,33 @@ COURSE_TEMPLATE_JAPANESE = [
     ModuleTemplate(words=90, level=90, lessons_types=['listening', 'vocabulary','reading', 'speaking'], lessons_content=[ 'verbs', 'nouns','adjectives','adverbs','writing','grammar', 'kanji'], lessons_count=10, module_count=5),
     ModuleTemplate(words=100, level=95, lessons_types=['listening', 'vocabulary','reading', 'speaking'], lessons_content=[ 'verbs', 'nouns','adjectives','adverbs','writing','syntax', 'kanji'], lessons_count=10, module_count=5),
     ModuleTemplate(words=200, level=100, lessons_types=['listening', 'vocabulary','reading', 'speaking'], lessons_content=[ 'verbs', 'nouns','adjectives','adverbs','writing','grammar', 'syntax', 'kanji'], lessons_count=10, module_count=5),
-]
+    ]
+)
 
 
 def gen_course_form_template(lang: str, to_lang: str):
-    template = COURSE_TEMPLATE_JAPANESE
+    template = COURSE_TEMPLATE_JAPANESE.modules
     module_count = 0
     for mt in template:
-        for i in range(mt.module_count):
+        lesson_count = 0
+        for _ in range(mt.module_count):
             module_count += 1
             print(f"Module: {module_count}  lessons_types: {mt.lessons_types}  lessons_content: {mt.lessons_content}")
             for lc in range(mt.lessons_count):
-
-                print( f"    Lesson: {lc+1}  lessons_type: {random.choice(mt.lessons_types)}  lessons_content: {random.choice(mt.lessons_content)}")
+                lesson_count+=1
+                lesson_type = mt.lessons_types[lesson_count%len(mt.lessons_types)]
+                lesson_content = mt.lessons_content[lesson_count%len(mt.lessons_content)]
+                print( f"    Lesson: {lc+1}  lessons_type: {lesson_type}  lessons_content: {lesson_content}")
+            print( f"    Lesson: {lc+2}  lessons_type: module recap")
             print("--------------------------------")
 
 
 
+def get_template_data(template: list[ModuleTemplate]):
+    pass 
+
+
 if __name__ == "__main__":
+
+    print(COURSE_TEMPLATE_JAPANESE.get_template_data())
     gen_course_form_template("en", "ja")
