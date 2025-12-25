a42f58e407db55180a98608d589c29d0c74fdd1f
David <davidvoler@gmail.com>
2025-12-25 14:57:16 +0200

    Getting words per module

--- editor/editor_server/src/generators/module_words.py
+++ editor/editor_server/src/generators/module_words.py
@@ -1,8 +1,6 @@
 from utils.db import get_query_results
 from pydantic import BaseModel
 
-from generators.course_template import CourseTemplate
-
 POS_EN =[
 'verb',
 'noun',
@@ -29,81 +27,79 @@ POS_JA =[
 
 class WordSelect(BaseModel):
     lang: str
-    pos: str
-    max_wcount: int
-    min_wcount: int
-    sentences_count: int
-    is_root: bool = False
+    word:str = ''
+    pos: str = ''
+    max_wcount: int = 0
+    min_wcount: int= 0
+    sentences_count: int = 0
+    root_count: int = 0
     def key(self):
-        return f"{self.lang}-{self.pos}"
+        return f"{self.lang}-{self.pos} "
 
 
-sentences_len_groups = [ 
-    (1, 3),
-    (4, 5),
-    (6, 7),
-    (8, 10),
-    (11, 16),
-    (17, 20),
-]
 
-async def _get_words_pos(lang: str,  max_wcount: int, min_wcount: int, pos: str=''):
-    sql = """
-    select word, pos, min(grp) as grp,sum(root_count) as root_count, sum(sentences_count) as sentences_count
+
+async def _get_words_pos(lang: str):
+    sql = f"""
+    select '{lang}' as lang, word, pos, min(min_wcount) as min_wcount, min(max_wcount) as max_wcount ,sum(root_count) as root_count, sum(sentences_count) as sentences_count
     from (
-    select word,pos,1 as grp, sum(root_count) as  root_count, sum(sentences_count) as sentences_count from content_raw.words_pos1 
-    where lang = 'ja' and wcount >=1 and wcount <=3 and sentences_count > 5
+    select word,pos,1 as min_wcount, 3 as max_wcount , sum(root_count) as  root_count, sum(sentences_count) as sentences_count from content_raw.words_pos1 
+    where lang = '{lang}' and wcount >=2 and wcount <=3 and sentences_count > 5
     group by 1,2
     union all
-    select word,pos,2 as grp,sum(root_count) as  root_count, sum(sentences_count) as sentences_count from content_raw.words_pos1 
-    where lang = 'ja' and wcount >=4 and wcount <=5 and sentences_count > 5
+    select word,pos,4 as min_wcount, 5 as max_wcount ,sum(root_count) as  root_count, sum(sentences_count) as sentences_count from content_raw.words_pos1 
+    where lang = '{lang}' and wcount >=4 and wcount <=5 and sentences_count > 5
     group by 1,2
     union all
-    select word,pos,3 as grp,sum(root_count) as  root_count, sum(sentences_count) as sentences_count from content_raw.words_pos1 
-    where lang = 'ja' and wcount >=6 and wcount <=7 and sentences_count > 5
+    select word,pos,6 as min_wcount, 7 as max_wcount ,sum(root_count) as  root_count, sum(sentences_count) as sentences_count from content_raw.words_pos1 
+    where lang = '{lang}' and wcount >=6 and wcount <=7 and sentences_count > 5
     group by 1,2
     union all
-    select word,pos as grp,4,sum(root_count) as  root_count, sum(sentences_count) as sentences_count from content_raw.words_pos1 
-    where lang = 'ja' and wcount >=8 and wcount <=10 and sentences_count > 5
+    select word,pos ,8 as min_wcount, 9 as max_wcount ,sum(root_count) as  root_count, sum(sentences_count) as sentences_count from content_raw.words_pos1 
+    where lang = '{lang}' and wcount >=8 and wcount <=10 and sentences_count > 5
     group by 1,2
     union all
-    select word,pos,5 as grp,sum(root_count) as  root_count, sum(sentences_count) as sentences_count from content_raw.words_pos1 
-    where lang = 'ja' and wcount >10  and sentences_count > 5
+    select word,pos,10 as min_wcount, 20 as max_wcount,sum(root_count) as  root_count, sum(sentences_count) as sentences_count from content_raw.words_pos1 
+    where lang = '{lang}' and wcount >=10 and wcount <= 20  and sentences_count > 5
     group by 1,2
     )
-    group by 1,2 
-    order by grp, root_count desc, sentences_count desc
+    group by 1,2,3
+    order by max_wcount, root_count desc, sentences_count desc
     """
+    results = await  get_query_results (sql, None)
+    words = []
+    for r in results:
+        words.append(WordSelect(**r))
+    return words
 
-async def _get_roots(lang: str,  max_wcount: int, min_wcount: int, pos: str=''):
-    pass
+class ModuleWords(BaseModel):
+    name: str = ''
+    num: int = 0
+    words:list[WordSelect] = []
+    word_count:int = 0
 
-
-async def get_all_words_by_sentences_len(template: CourseTemplate):
-    """Select words for a module based on the template"""
-    all_words = {f"{len_group[0]}-{len_group[1]}": {} for len_group in sentences_len_groups}
+async def get_all_words_by_sentences_len(lang):
     
-    for len_group in sentences_len_groups:
-        words_root = await _get_roots(template.lang, len_group[0], len_group[1])
-        for w in words_root:
-            all_words[f"{len_group[0]}-{len_group[1]}"][w.key()] = w
-        words = await _get_words_pos(template.lang, len_group[0], len_group[1])
-        for w in words:
-            if w.key() not in all_words[f"{len_group[0]}-{len_group[1]}"]:
-                all_words[f"{len_group[0]}-{len_group[1]}"][w.key()] = w
+    """Select words for a module based on the template"""
+    all_words = await _get_words_pos(lang)
+    modules = []
+    start_words_per_module = 5
+    ratio_increase = 0.08
+    modules = []
+    for i in range(1000):
+        if len(all_words) == 0 :
+            break
+        word_count = int(round(start_words_per_module + i*ratio_increase))
+        m = ModuleWords(
+            name = f'module {i}',
+            num = i,
+            words = all_words[:word_count],
+            word_count = word_count
+        )
+        all_words = all_words[word_count:]
+        modules.append(m)
+    return modules
 
-    return all_words
 
 
-"""
-
-TODO:
-order the words in some logic
-create module 
--assgin words to module 
-return a list of module 
-
-
-
-"""
-
+    
