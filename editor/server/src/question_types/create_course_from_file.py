afc4d304208bb25e35ddf6050f38a7ee63a0c3e2
David <davidvoler@gmail.com>
2026-01-10 09:14:55 +0200

    reading from file as the table was deleted

--- editor/server/src/question_types/create_course_from_file.py
+++ editor/server/src/question_types/create_course_from_file.py
@@ -1,5 +1,16 @@
 from utils.db import get_query_results
 import json
+WORDS_SO_FAR = set()
+WORDS_IN_SENTENCE = set()
+HIRAGANA_SO_FAR = set()
+KATAKANA_SO_FAR = set()
+KANJI_SO_FAR = set()
+
+
+async def augment_word_lesson(lesson: dict):
+    pass 
+async def augment_alphabet_lesson(lesson: dict):
+    pass
 
 
 async def create_course(lang, to_lang, title, description):
@@ -27,6 +38,7 @@ async def create_lesson(lang, to_lang, title, description, course_id, module_id)
     return res[0].get('lesson_id')
 
 async def create_exercise(lang, to_lang, course_id, module_id,lesson_id, sentence_id, to_sentence_id,exercise_type="sentence",extra_data=[]):
+    
     sql = """
     INSERT INTO content.exercise(lang,to_lang,course_id, module_id,lesson_id, sentence_id, to_sentence_id,exercise_type,extra_data) 
     VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s) RETURNING exercise_id
@@ -58,17 +70,32 @@ async def create_course_from_file(path,lang, to_lang, title, description):
     course_id = await create_course(lang, to_lang, title, description)
     module_id = 0
     lesson_id = 0
+    word = ''
+    ab_type = ''
+    letter = ''
     for e in elements:
         l = e
         if l[0] == "M":
             title = " ".join(l[1:]).replace("by_words","words")
             module_id = await create_module(lang, to_lang, title, title, course_id)
         elif l[0] == "L":
-            title = " ".join(l[1:]).replace("by_words","").strip()
-            lesson_id = await create_lesson(lang, to_lang, title, title, course_id,module_id)
+            if l[1] in ["greeting", "by_words"]:
+                word = l[2]
+                WORDS_SO_FAR.add(word)
+                title = " ".join(l[1:]).replace("by_words","").strip()
+                lesson_id = await create_lesson(lang, to_lang, title, title, course_id,module_id)
+            elif l[1] in ["kanji","katakana",'hiragana']:
+                letter = l[3]
+                ab_type = l[2]
+                if ab_type == "hiragana":
+                    HIRAGANA_SO_FAR.add(letter)
+                elif ab_type == "katakana":
+                    KATAKANA_SO_FAR.add(letter)
+                elif ab_type == "kanji":
+                    KANJI_SO_FAR.add(letter)
         elif l[0] == "S":
             if l[1] in ["kanji","katakana",'hiragana']:
-                await create_exercise(lang, to_lang,course_id, module_id,lesson_id, 0, 0 ,"alphabet",l)
+                await create_ab_exercise(lang, to_lang,course_id, module_id,lesson_id, 0, 0 ,"alphabet",l)
             else:
                await create_exercise(lang, to_lang,course_id, module_id,lesson_id, l[2], l[5])
 
