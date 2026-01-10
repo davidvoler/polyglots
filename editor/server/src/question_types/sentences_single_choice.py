8244b4bd47fef4922b223d6e8ae47e5ba78c1d7d
David <davidvoler@gmail.com>
2026-01-10 18:55:24 +0200

    single option is working

--- editor/server/src/question_types/sentences_single_choice.py
+++ editor/server/src/question_types/sentences_single_choice.py
@@ -5,7 +5,7 @@ from question_types.exercise_model import ExerciseModel, save_exercise_new_forma
 
 async def single_choice_sentence(exercise:ExerciseModel, sentence: dict, to_sentence: dict) -> int:
     exercise.correct_options = [to_sentence.get('text')]
-    exercise.wrong_options = [to_sentence.get('options')]
+    exercise.wrong_options = to_sentence.get('options')
     exercise.exercise_type = 'sentence_single_choice'
     await save_exercise_new_format(exercise)
     
